1555e18d96552ce17dbbc76b8111019fd94ca2a2
John Lathouwers <john.lathouwers@oracle.com>
2024-04-09 11:55:44 +0100

    default configMap

--- internal/controller/restdataservices_controller.go
+++ internal/controller/restdataservices_controller.go
@@ -3,11 +3,13 @@ package controller
 import (
 	"context"
 	"fmt"
-	"strings"
+
+	//"strings"
 	"time"
 
-	appsv1 "k8s.io/api/apps/v1"
+	//appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	meta "k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -40,187 +42,145 @@ type RestDataServicesReconciler struct {
 //+kubebuilder:rbac:groups=database.oracle.com,resources=restdataservices/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=database.oracle.com,resources=restdataservices/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=configmaps/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
 func (r *RestDataServicesReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	logger := log.FromContext(ctx)
+	logr := log.FromContext(ctx)
 	ords := &databasev1.RestDataServices{}
 
 	// Check if there is an ORDS resource; if not nothing to reconcile
 	if err := r.Get(ctx, req.NamespacedName, ords); err != nil {
 		if apierrors.IsNotFound(err) {
+			logr.Info("No RestDataServices resources found")
 			return ctrl.Result{}, nil
 		}
-		logger.Error(err, "Unable to retrieve an RestDataServices CRD.", "connection", ords)
-		return ctrl.Result{}, err
+		// The CR is not defined... something has gone horribly wrong!!
+		logr.Error(err, "Unable to retrieve an RestDataServices CRD.", "connection", ords)
+		return ctrl.Result{RequeueAfter: time.Minute}, err
 	}
 
 	// Set the status as Unknown when no status are available
 	if ords.Status.Conditions == nil || len(ords.Status.Conditions) == 0 {
-		meta.SetStatusCondition(&ords.Status.Conditions, metav1.Condition{Type: typeAvailable, Status: metav1.ConditionUnknown, Reason: "Reconciling", Message: "Starting reconciliation"})
-		if err := r.Status().Update(ctx, ords); err != nil {
-			logger.Error(err, "Failed to update", "obj", ords)
-			return ctrl.Result{}, err
+		condition := metav1.Condition{
+			Type: typeAvailable, Status: metav1.ConditionUnknown,
+			Reason: "Reconciling", Message: "Starting reconciliation",
 		}
-		if err := r.Get(ctx, req.NamespacedName, ords); err != nil {
-			logger.Error(err, "Failed to re-fetch", "obj", ords)
-			return ctrl.Result{}, err
-		}
+		err := r.updateStatus(ctx, req, ords, condition)
+		return ctrl.Result{}, err
 	}
 
-	// Check if the workload already exists, if not create a new one
-	found := &appsv1.Deployment{}
-	err := r.Get(ctx, types.NamespacedName{Name: ords.Name, Namespace: ords.Namespace}, found)
+	// Default ConfigMap
+	existingConfigMap := &corev1.ConfigMap{}
+	err := r.Get(ctx, types.NamespacedName{Name: "ords-default-config", Namespace: ords.Namespace}, existingConfigMap)
 	if err != nil && apierrors.IsNotFound(err) {
-		// Define a new deployment
-		dep, err := r.deploymentForRestDataServices(ords)
+		logr.Info("Missing Default ConfigMap, Creating")
+		def, err := r.defConfigMap(ctx, ords)
 		if err != nil {
-			logger.Error(err, "Failed to define new Deployment resource for RestDataServices")
-
-			// The following implementation will update the status
-			meta.SetStatusCondition(&ords.Status.Conditions, metav1.Condition{Type: typeAvailable,
-				Status: metav1.ConditionFalse, Reason: "Reconciling",
-				Message: fmt.Sprintf("Failed to create Deployment for the custom resource (%s): (%s)", ords.Name, err)})
-
-			if err := r.Status().Update(ctx, ords); err != nil {
-				logger.Error(err, "Failed to update RestDataServices status")
-				return ctrl.Result{}, err
+			logr.Error(err, "Failed to define new ConfigMap for RestDataServices")
+			condition := metav1.Condition{
+				Type: typeAvailable, Status: metav1.ConditionFalse,
+				Reason: "RequirementsNotMet", Message: "Default ConfigMap does not exist",
 			}
-
+			err := r.updateStatus(ctx, req, ords, condition)
 			return ctrl.Result{}, err
 		}
-
-		logger.Info("Creating a new Deployment",
-			"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-		if err = r.Create(ctx, dep); err != nil {
-			logger.Error(err, "Failed to create new Deployment",
-				"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		logr.Info("Creating ConfigMap", "Namespace", def.Namespace, "Name", def.Name)
+		if err = r.Create(ctx, def); err != nil {
+			logr.Error(err, "Failed creating new ConfigMap", "Namespace", def.Namespace, "Name", def.Name)
 			return ctrl.Result{}, err
 		}
-
-		// Deployment created successfully
-		// We will requeue the reconciliation so that we can ensure the state
-		// and move forward for the next operations
-		return ctrl.Result{RequeueAfter: time.Minute}, nil
-	} else if err != nil {
-		logger.Error(err, "Failed to get Deployment")
-		// Let's return the error for the reconciliation be re-trigged again
-		return ctrl.Result{}, err
-	}
-
-	// Reconcile Replicas
-	replicas := ords.Spec.Replicas
-	if *found.Spec.Replicas != replicas {
-		logger.Info("Scaling", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
-		found.Spec.Replicas = &replicas
-		if err = r.Update(ctx, found); err != nil {
-			logger.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
-
-			if err := r.Get(ctx, req.NamespacedName, ords); err != nil {
-				logger.Error(err, "Failed to re-fetch ords")
-				return ctrl.Result{}, err
+	} else {
+		logr.Info("Found Default ConfigMap, Reconciling")
+		newConfigMap, err := r.defConfigMap(ctx, ords)
+		if err != nil {
+			logr.Error(err, "Failed to define comparable ConfigMap for RestDataServices")
+			condition := metav1.Condition{
+				Type: typeAvailable, Status: metav1.ConditionFalse,
+				Reason: "ResourceFound", Message: "Starting ConfigMap Reconciliation",
 			}
-
-			// The following implementation will update the status
-			meta.SetStatusCondition(&ords.Status.Conditions, metav1.Condition{Type: typeAvailable,
-				Status: metav1.ConditionFalse, Reason: "Resizing",
-				Message: fmt.Sprintf("Failed to update the size for the custom resource (%s): (%s)", ords.Name, err)})
-
-			if err := r.Status().Update(ctx, ords); err != nil {
-				logger.Error(err, "Failed to update ords status")
-				return ctrl.Result{}, err
-			}
-
+			err := r.updateStatus(ctx, req, ords, condition)
 			return ctrl.Result{}, err
 		}
-		return ctrl.Result{Requeue: true}, nil
+		if equality.Semantic.DeepEqual(existingConfigMap.Data, newConfigMap.Data) {
+			logr.Info("ConfigMaps are the same. No action needed.")
+			return ctrl.Result{}, nil
+		}
+		logr.Info("Updating ConfigMap", "Namespace", newConfigMap.Namespace, "Name", newConfigMap.Name)
+		if err := r.Update(ctx, newConfigMap); err != nil {
+			logr.Error(err, "Failed updating ConfigMap", "Namespace", newConfigMap.Namespace, "Name", newConfigMap.Name)
+			return ctrl.Result{}, err
+		}
 	}
 
-	meta.SetStatusCondition(&ords.Status.Conditions, metav1.Condition{Type: typeAvailable,
-		Status: metav1.ConditionTrue, Reason: "Reconciling",
-		Message: fmt.Sprintf("Deployment for custom resource (%s) with %d replicas created successfully", ords.Name, replicas)})
-
-	if err := r.Status().Update(ctx, ords); err != nil {
-		logger.Error(err, "Failed to update status", "obj", ords)
-		return ctrl.Result{}, err
-	}
-
-	return ctrl.Result{}, nil
+	// Set CR Status
+	condition := metav1.Condition{Type: typeAvailable, Status: metav1.ConditionTrue,
+		Reason: "Succeeded", Message: fmt.Sprintf("Resource (%s) created successfully", ords.Name)}
+	err = r.updateStatus(ctx, req, ords, condition)
+	return ctrl.Result{}, err
 }
 
-// deployment
-func (r *RestDataServicesReconciler) deploymentForRestDataServices(ords *databasev1.RestDataServices) (*appsv1.Deployment, error) {
-	ls := labelsForRestDataServices(ords.Name, ords.Spec.Image)
-	podTemplate := podForRestDataServices(ords)
-	replicas := ords.Spec.Replicas
+// UpdateStatus
+func (r *RestDataServicesReconciler) updateStatus(ctx context.Context, req ctrl.Request, ords *databasev1.RestDataServices, condition metav1.Condition) error {
+	logr := log.FromContext(ctx).WithName("updateStatus")
+	meta.SetStatusCondition(&ords.Status.Conditions, condition)
+	if err := r.Status().Update(ctx, ords); err != nil {
+		logr.Error(err, "Failed to update RestDataServices status")
+		return err
+	}
+	if err := r.Get(ctx, req.NamespacedName, ords); err != nil {
+		logr.Error(err, "Failed to re-fetch RestDataServices")
+		return err
+	}
+	return nil
+}
 
-	dep := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      ords.Name,
-			Namespace: ords.Namespace,
+// ConfigMaps
+func (r *RestDataServicesReconciler) defConfigMap(ctx context.Context, ords *databasev1.RestDataServices) (*corev1.ConfigMap, error) {
+	ls := labelsForRestDataServices(ords.Name)
+	def := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
 		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &replicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: ls,
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: ls,
-				},
-				Spec: podTemplate,
-			},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "ords-default-config",
+			Namespace: ords.Namespace,
+			Labels:    ls,
+		},
+		Data: map[string]string{
+			"settings.xml": `<?xml version="1.0" encoding="UTF-8"?>
+							<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
+							<properties>
+							<comment>Default created by Controller</comment>
+							<entry key="database.api.enabled">false</entry>
+							<entry key="debug.printDebugToScreen">false</entry>
+							<entry key="feature.sdw">false</entry>
+							<entry key="jdbc.InitialLimit">10</entry>
+							<entry key="jdbc.MaxLimit">100</entry>
+							<entry key="logr.procedure">false</entry>
+							<entry key="misc.defaultPage">apex</entry>
+							<entry key="restEnabledSql.active">false</entry>
+							<entry key="security.httpsHeaderCheck">X-Forwarded-Proto: https</entry>
+							<entry key="standalone.context.path">/</entry>
+							<entry key="standalone.http.port">8080</entry>
+							<entry key="standalone.static.context.path">/i</entry>
+							</properties>`,
 		},
 	}
 
-	// Set the ownerRef for the Deployment
-	// More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/owners-dependents/
-	if err := ctrl.SetControllerReference(ords, dep, r.Scheme); err != nil {
+	// Set the ownerRef
+	if err := ctrl.SetControllerReference(ords, def, r.Scheme); err != nil {
 		return nil, err
 	}
-	return dep, nil
+	return def, nil
 }
 
-func podForRestDataServices(ords *databasev1.RestDataServices) corev1.PodSpec {
-	podTemplate := corev1.PodSpec{
-		SecurityContext: &corev1.PodSecurityContext{
-			RunAsNonRoot: &[]bool{true}[0],
-			SeccompProfile: &corev1.SeccompProfile{
-				Type: corev1.SeccompProfileTypeRuntimeDefault,
-			},
-		},
-		Containers: []corev1.Container{{
-			Image:           ords.Spec.Image,
-			Name:            fmt.Sprintf("%s-ords", ords.Name),
-			ImagePullPolicy: corev1.PullIfNotPresent,
-			SecurityContext: &corev1.SecurityContext{
-				RunAsNonRoot:             &[]bool{true}[0],
-				RunAsUser:                &[]int64{54321}[0],
-				AllowPrivilegeEscalation: &[]bool{false}[0],
-				Capabilities: &corev1.Capabilities{
-					Drop: []corev1.Capability{
-						"ALL",
-					},
-				},
-			},
-			Ports: []corev1.ContainerPort{{
-				ContainerPort: ords.Spec.Port,
-				Name:          "standalone-port",
-			}},
-			Command: []string{"ords", "serve"},
-		}},
-	}
-	return podTemplate
-}
-
-func labelsForRestDataServices(name string, image string) map[string]string {
-	var imageTag string
-	imageTag = strings.Split(image, ":")[1]
-
+func labelsForRestDataServices(name string) map[string]string {
 	return map[string]string{"app.kubernetes.io/name": "ORDS",
 		"app.kubernetes.io/instance":   name,
-		"app.kubernetes.io/version":    imageTag,
 		"app.kubernetes.io/part-of":    "oracle-ords-operator",
 		"app.kubernetes.io/created-by": "controller-manager",
 	}
@@ -230,5 +190,6 @@ func labelsForRestDataServices(name string, image string) map[string]string {
 func (r *RestDataServicesReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&databasev1.RestDataServices{}).
+		Owns(&corev1.ConfigMap{}).
 		Complete(r)
 }
